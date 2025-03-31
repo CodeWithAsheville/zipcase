8a34bb13f80e112d357a49c7e3f99b01d840a5e5
Jay Hill <116148+jayhill@users.noreply.github.com>
2025-03-31 12:44:18 -0400

    #9 allow terraform apply to run in CI/CD without an AWS profile

--- infra/terraform/bootstrap/variables.tf
+++ infra/terraform/bootstrap/variables.tf
@@ -7,4 +7,11 @@ variable "aws_region" {
 variable "aws_profile" {
   description = "The AWS CLI profile to use for authentication"
   type        = string
+  default     = null
+}
+
+variable "use_profile" {
+  description = "Whether to use AWS profile or direct credentials"
+  type        = bool
+  default     = true
 }
