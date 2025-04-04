def6c23edfca60ae1f354f32ca1567b939b8d687
Jay Hill <116148+jayhill@users.noreply.github.com>
2025-04-04 08:41:54 -0400

    #9 add identityPoolId to frontend aws-exports

--- frontend/src/aws-exports.ts
+++ frontend/src/aws-exports.ts
@@ -15,8 +15,8 @@ const awsExports: ResourcesConfig = {
         Cognito: {
             userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
             userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-        },
-        mandatorySignIn: true,
+            identityPoolId: import.meta.env.VITE_COGNITO_IDENTITY_POOL_ID || undefined,
+        }
     },
 };
 
