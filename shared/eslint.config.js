1460b86c6b4b198b1c63e046d0252bde381e0172
Jay Hill <116148+jayhill@users.noreply.github.com>
2025-04-15 06:37:32 -0400

    #32 set up linting of shared project

---
+++ shared/eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    { 
+        ignores: [
+            'dist', 
+            'node_modules'
+        ] 
+    },
+    // Base configuration for all files
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,js}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: {
+                ...globals.node
+            },
+        },
+    }
+);
